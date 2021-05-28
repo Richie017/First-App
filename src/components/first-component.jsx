f876e4da71582df62ad9cedb2ba2e6a5364862a4
Raphael <richie.rozario07@gmail.com>
2021-05-28 23:03:06 +0600

    [Add] [implemented single component]

--- src/components/first-component.jsx
+++ src/components/first-component.jsx
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class First extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return <h1>Hello Richie</h1>;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  styles = {
+    textAlign: "center",
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1 style={this.styles}>Hello Richie</h1>
+        <span className={this.getBadgeStyle()}>{this.formatCount()}</span>
+        <button className="btn btn-info btn-sm m-2">+</button>
+        <button className="btn btn-danger btn-sm">-</button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeStyle() {
+    let badges = "badge m-3 bg-";
+    badges += this.state.count === 0 ? "warning" : "primary";
+    return badges;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default First;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
 import First from "./components/first-component"
+// import Header from "./components/header";
 
 // const element = <h1>Hello World</h1>
 // ReactDOM.render(element, document.getElementById("root"));
+// ReactDOM.render(<Header />, document.getElementById("root"));
 ReactDOM.render(<First />, document.getElementById("root"));
