fc940ffb919657774f82afb6f033cd454c69ed52
Richie017 <richie.rozario07@gmail.com>
2021-05-28 19:28:30 +0600

    [Add] [initial setup]

---
+++ src/components/first-component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class First extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return <h1>Hello Richie</h1>;
+    }
+}
+ 
+export default First;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import First from "./components/first-component"
+
+// const element = <h1>Hello World</h1>
+// ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<First />, document.getElementById("root"));
